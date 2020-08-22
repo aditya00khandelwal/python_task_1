0e7de844e79bda7b931c9f2fc22a270fcb974621
aditya00khandelwal <64269832+aditya00khandelwal@users.noreply.github.com>
2020-08-22 23:18:38 +0530

    Update aditya.py

--- aditya.py
+++ aditya.py
@@ -2,12 +2,14 @@
 import pyttsx3
 import os
 
-pyttsx3.speak("Welcome to my software")
-print("Welcome to my software")
+pyttsx3.speak(" Hey...Welcome to my software")
+x = "Welcome to my software "
+y = x.center(80)
+print(y)
 
 while True:
-	pyttsx3.speak("How can I help you")
-	print("How can I help you: ", end='')
+	pyttsx3.speak("How may I assist you")
+	print("How may I assist you: ", end='')
 	p=input()
 
 	if (("run" in p) or ("launch" in p) or ("open" in p) or ("execute" in p)) and (("chrome" in p) or ("google" in p)):
@@ -18,7 +20,7 @@ while True:
 			pyttsx3.speak("opening  google chrome")
 			os.system("start chrome")
 
-	elif (("run" in p) or ("launch" in p) or ("open" 	in p) or ("execute" in p)) and (("notepad" in p) or("notebook" in p) or ("editor" in p)):
+	elif (("run" in p) or ("launch" in p) or ("open" in p) or ("execute" in p)) and (("notepad" in p) or("notebook" in p) or ("editor" in p)):
 		
 		if (("dont" in p) or ("not" in p) or ("don't" in p)):
 			print("", end='')
@@ -27,7 +29,7 @@ while True:
 			pyttsx3.speak("opening notepad")
 			os.system("notepad")
 
-	elif (("run" in p) or ("launch" in p) or 	("execute" in p) or ("open" in p)) and (("media" in p) and ("player" in p)):
+	elif (("run" in p) or ("launch" in p) or ("execute" in p) or ("open" in p)) and (("media" in p) and ("player" in p)):
 		
 		if (("dont" in p) or ("not" in p) or ("don't" in p)):
 			print("", end='')
@@ -36,7 +38,7 @@ while True:
 			pyttsx3.speak("opening windows media player")
 			os.system("start wmplayer")
 
-	elif (("run" in p) or ("launch" in p) or 	("execute" in p) or ("open" in p)) and (("explorer" in p) or ("browser" in p)):
+	elif (("run" in p) or ("launch" in p) or ("execute" in p) or ("open" in p)) and (("explorer" in p) or ("browser" in p)):
 
 		if (("dont" in p) or ("not" in p) or ("don't" in p)):
 			print("", end='')
@@ -45,7 +47,7 @@ while True:
 			pyttsx3.speak("opening internet explorer")
 			os.system("start iexplore")
 
-	elif (("run" in p) or ("launch" in p) or 	("execute" in p) or ("open" in p)) and (("outlook" in p)):
+	elif (("run" in p) or ("launch" in p) or ("execute" in p) or ("open" in p)) and (("outlook" in p)):
 		if (("dont" in p) or ("not" in p) or ("don't" in p)):
 			print("", end='')
 		else:
@@ -53,63 +55,63 @@ while True:
 			pyttsx3.speak("opening microsoft outlook")
 			os.system("start outlook")
 
-	elif (("run" in p) or ("launch" in p) or 	("execute" in p) or ("open" in p)) and (("powerpoint" in p)):
+	elif (("run" in p) or ("launch" in p) or ("execute" in p) or ("open" in p)) and (("powerpoint" in p)):
 		if (("dont" in p) or ("not" in p) or ("don't" in p)):
 			print("", end='')
 		else:
 			pyttsx3.speak("opening microsoft powerpoint")
 			os.system("start powerpnt")
 
-	elif (("run" in p) or ("launch" in p) or 	("execute" in p) or ("open" in p)) and (("computer" in p) or ("pc" in p)):
+	elif (("run" in p) or ("launch" in p) or ("execute" in p) or ("open" in p)) and (("computer" in p) or ("pc" in p)):
 		if (("dont" in p) or ("not" in p) or ("don't" in p)):
 			print("", end='')
 		else:
 			pyttsx3.speak("opening my computer")
 			os.system("start explorer")
 
-	elif (("run" in p) or ("launch" in p) or 	("execute" in p) or ("open" in p)) and (("calculator" in p) or ("calculations" in p)):
+	elif (("run" in p) or ("launch" in p) or ("execute" in p) or ("open" in p)) and (("calculator" in p) or ("calculations" in p)):
 		if (("dont" in p) or ("not" in p) or ("don't" in p)):
 			print("", end='')
 		else:
 			pyttsx3.speak("opening calculator")
 			os.system("calc")
 
-	elif (("run" in p) or ("launch" in p) or 	("execute" in p) or ("open" in p)) and (("magnifier" in p) or ("zoom" in p)):
+	elif (("run" in p) or ("launch" in p) or ("execute" in p) or ("open" in p)) and (("magnifier" in p) or ("zoom" in p)):
 		if (("dont" in p) or ("not" in p) or ("don't" in p)):
 			print("", end='')
 		else:
 			pyttsx3.speak("opening magnifier")
 			os.system("start magnify")
 
-	elif (("run" in p) or ("launch" in p) or 	("execute" in p) or ("open" in p)) and (("paint" in p) or ("draw" in p)):
+	elif (("run" in p) or ("launch" in p) or ("execute" in p) or ("open" in p)) and (("paint" in p) or ("draw" in p)):
 		if (("dont" in p) or ("not" in p) or ("don't" in p)):
 			print("", end='')
 		else:
 			pyttsx3.speak("opening paint")
 			os.system("start mspaint")
 
-	elif (("run" in p) or ("launch" in p) or 	("execute" in p) or ("open" in p)) and (("registry" in p) and ("editor" in p)):
+	elif (("run" in p) or ("launch" in p) or ("execute" in p) or ("open" in p)) and (("registry" in p) and ("editor" in p)):
 		if (("dont" in p) or ("not" in p) or ("don't" in p)):
 			print("", end='')
 		else:
 			pyttsx3.speak("opening registry editor")
 			os.system("start regedit")
 
-	elif (("run" in p) or ("launch" in p) or 	("execute" in p) or ("open" in p)) and (("wordpad" in p) or ("word" in p)):
+	elif (("run" in p) or ("launch" in p) or ("execute" in p) or ("open" in p)) and (("wordpad" in p) or ("word" in p)):
 		if (("dont" in p) or ("not" in p) or ("don't" in p)):
 			print("", end='')
 		else:
 			pyttsx3.speak("opening wordpad")
 			os.system("start write")
 
-	elif (("run" in p) or ("launch" in p) or 	("execute" in p) or ("open" in p)) and (("system" in p) and ("configuration" in p)):
+	elif (("run" in p) or ("launch" in p) or ("execute" in p) or ("open" in p)) and (("system" in p) and ("configuration" in p)):
 		if (("dont" in p) or ("not" in p) or ("don't" in p)):
 			print("", end='')
 		else:
 			pyttsx3.speak("opening system configuration")
 			os.system("msconfig")
 	
-	elif (("run" in p) or ("launch" in p) or 	("execute" in p) or ("open" in p)) and (("command" in p) and ("promt" in p) or ("cmd" in p)):
+	elif (("run" in p) or ("launch" in p) or ("execute" in p) or ("open" in p)) and (("command" in p) and ("promt" in p) or ("cmd" in p)):
 		if (("dont" in p) or ("not" in p) or ("don't" in p)):
 			print("", end='')
 		else:
@@ -123,37 +125,37 @@ while True:
 			path = input("Enter thr name of Folder:")
 			pyttsx3.speak("opening path") 
 			os.mkdir("path")
-	elif (("run" in p) or ("launch" in p) or 	("execute" in p) or ("open" in p)) and (("whatsapp" in p)):
+	elif (("run" in p) or ("launch" in p) or ("execute" in p) or ("open" in p)) and (("whatsapp" in p)):
 		if (("dont" in p) or ("not" in p) or ("don't" in p)):
 			print("", end='')
 		else:
 			pyttsx3.speak("opening whatsapp") 
 			os.system("whatsapp.lnk")
-	elif (("run" in p) or ("launch" in p) or 	("execute" in p) or ("open" in p)) and (("tasklist" in p)):
+	elif (("run" in p) or ("launch" in p) or ("execute" in p) or ("open" in p)) and (("tasklist" in p)):
 		if (("dont" in p) or ("not" in p) or ("don't" in p)):
 			print("", end='')
 		else:
 			pyttsx3.speak("opening tasklist") 
 			os.system("tasklist")
-	elif (("run" in p) or ("launch" in p) or 	("execute" in p) or ("open" in p)) and (("excel" in p)):
+	elif (("run" in p) or ("launch" in p) or ("execute" in p) or ("open" in p)) and (("excel" in p)):
 		if (("dont" in p) or ("not" in p) or ("don't" in p)):
 			print("", end='')
 		else:
 			pyttsx3.speak("opening microsoft excel") 
 			os.system("start excel.exe")
-	elif (("run" in p) or ("launch" in p) or 	("execute" in p) or ("open" in p)) and (("control" in p) and ("panel" in p)):
+	elif (("run" in p) or ("launch" in p) or ("execute" in p) or ("open" in p)) and (("control" in p) and ("panel" in p)):
 		if (("dont" in p) or ("not" in p) or ("don't" in p)):
 			print("", end='')
 		else:
 			pyttsx3.speak("opening control panel") 
 			os.system("control panel")
-	elif (("run" in p) or ("launch" in p) or 	("execute" in p) or ("open" in p) or ("show" in p)) and (("date" in p)):
+	elif (("run" in p) or ("launch" in p) or ("execute" in p) or ("open" in p) or ("show" in p)) and (("date" in p)):
 		if (("dont" in p) or ("not" in p) or ("don't" in p)):
 			print("", end='')
 		else:
 			pyttsx3.speak("showing today's date") 
 			os.system("date")
-	elif (("run" in p) or ("launch" in p) or 	("execute" in p) or ("open" in p) or ("show" in p)) and (("time" in p)):
+	elif (("run" in p) or ("launch" in p) or ("execute" in p) or ("open" in p) or ("show" in p)) and (("time" in p)):
 		if (("dont" in p) or ("not" in p) or ("don't" in p)):
 			print("", end='')
 		else:
@@ -165,5 +167,5 @@ while True:
 		break
 
 	else:
-		pyttsx3.speak("don't support")
+		pyttsx3.speak("currently not available")
 
